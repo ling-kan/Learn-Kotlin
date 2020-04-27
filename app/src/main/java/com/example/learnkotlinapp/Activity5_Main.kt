64cf680cc414ea7c09494cce30b065e67d4907ee
lingkn <l.kan@reply.com>
2020-04-27 13:42:25 +0100

    Activity 5

---
+++ app/src/main/java/com/example/learnkotlinapp/Activity5_Main.kt
@@ -0,0 +1,12 @@
+package com.example.learnkotlinapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity5_Main : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity5__main)
+    }
+}
